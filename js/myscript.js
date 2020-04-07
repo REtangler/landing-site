12c72bc31e443f54b40223fdd2152567ea118f4f
Yurii Lysak <lysak696@gmail.com>
2020-04-08 00:45:36 +0300

    a little bit of adapting

--- js/myscript.js
+++ js/myscript.js
@@ -16,16 +16,32 @@ function topFunction() {
     document.documentElement.scrollTop = 0;
 }
 // Changing bg color of field
-function myFunction(x) {
-  x.style.background = "burlywood";
-}
+// function myFunction(x) {
+//   x.style.background = "burlywood";
+// }
 
-function text_alert(){
-    alert("I am an alert box!");
-}
+// function text_alert(){
+//     alert("I am an alert box!");
+// }
 
 function scroll_sec1() {
     var el = document.getElementById("sec1");
     el.scrollIntoView({behavior: "smooth"});
     // window.scrollTo(0,document.getElementById("sec1"));
+}
+
+// function syncplay() {
+//     var video = document.getElementById("title-video")
+//     video.oncanplay = document.getElementById("title-blur-video").onplay();
+// }
+function syncplay() {   //якшо передній відос повністю займає екран, задній не запускати
+    var video = document.getElementById("title-video");
+    // var video_blur = document.getElementById("title-blur-video");
+    video.addEventListener("canplay", function() {
+      setTimeout(function() {
+        video.play();
+        video = document.getElementById("title-blur-video");
+        video.play();
+      }, 100);
+    }); 
 }
